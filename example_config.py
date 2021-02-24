3cfb5d9ba6dbcd30c554ce5774e9f125d03bfbaf
SJO_8164 <17802719+sjo-8164@users.noreply.github.com>
2021-02-24 04:54:18 +0000

    Fix config file formatting and typos

--- example_config.py
+++ example_config.py
@@ -3,7 +3,7 @@
 # Horus UDP to BaseStation config
 #
 
-# Port for listening to incomming Horus UDP packets
+# Port for listening to incoming Horus UDP packets
 HORUS_UDP_PORT = 55673
 
 # Basestation output parameters
@@ -11,5 +11,5 @@ OUTPUT_IP = '192.168.1.100'
 OUTPUT_PORT = 30000
 
 # ICAO file parameters
-ICAO_FILE = "/path/to/icaos.json"		# File to store history of matches between ICAOs and radiosonde serial numbers
-ICAO_LIMIT = 100				# Number of matches to keep stored in specified file
+ICAO_FILE = "icaos.json"  # File to store history of matches between ICAOs and radiosonde serial numbers
+ICAO_LIMIT = 100  # Number of matches to keep stored in specified file
